f96621ecb3d4a8f598c4a41367b3f1bb1cc11e2e
Igor Oliveira <igor.clinton.007@gmail.com>
2023-07-17 18:44:25 -0300

    models

---
+++ app/Enums/Status.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum Status: String
+{
+    CASE PENDING = 'P';
+    CASE APPROVED = 'A';
+    CASE CANCELLED = 'C';
+}
---
+++ app/Models/Sale.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Sale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'client_id',
+        'seller_id',
+        'status',
+        'total_amount'
+    ];
+
+    protected $casts = [
+        'status' => Status::class
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function seller(): BelongsTo
+    {
+        return $this->belongsTo(Seller::class);
+    }
+}
