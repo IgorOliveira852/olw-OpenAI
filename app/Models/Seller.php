649cbacf12db858424b26a13e0ba988c6b7b98b3
Igor Oliveira <igor.clinton.007@gmail.com>
2023-07-17 18:54:03 -0300

    factories

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Saller extends Model
+class Seller extends Model
 {
     use HasFactory;
 
